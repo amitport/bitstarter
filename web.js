e74608536c6ccb31b14a9340d299161599c90b3a
Amit Portnoy <example@example.com>
2013-07-10 18:31:34 +0000

    new from index

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs = require("fs");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var b = fs.readFileSync("index.html", 'ascii');
+  response.send(b);
 });
 
 var port = process.env.PORT || 5000;
